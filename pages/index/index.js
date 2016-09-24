1f6e8421dd69f03f18ed3c485d6e1579bd940506
chendaole <1174250185@qq.com>
2016-09-25 02:51:39 +0800

    fix:修正demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,36 @@
 //index.js
 //获取应用实例
 var app = getApp()
+const imagesUrl = [
+  "/images/carousel-A.png",
+  "/images/carousel-B.png",
+  "/images/carousel-C.png"
+]
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
+  data:{
+    carousel : {
+      urlIndex: 0,
+      url: imagesUrl[0]
+    }
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-  	//调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-      that.update()
-    })
+  onReady:function(){
+    setInterval(() => {
+      const imagesLength = imagesUrl.length;
+      const urlIndex  = this.data.carousel.urlIndex + 1 > imagesLength ? 0 : this.data.carousel.urlIndex + 1;
+      this.setData({carousel:{urlIndex: urlIndex, url: imagesUrl[urlIndex]}});
+    }, 2 * 1000);
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
   }
 })
